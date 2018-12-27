898d9e1a2fbb6819803097de81fcae19972cea1e
Carson Beus <carson.beus@gmail.com>
2018-12-27 11:05:52 -0700

    first commit

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ sah: 'dude' });
-});
+app.listen(5000);
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: keys.googleClientID,
+            clientSecret: keys.googleClientSecret,
+            callbackURL: '/auth/google/callback'
+        }, 
+        (accessToken) => {
+            console.log(accessToken);
+        }
+    )   
+);
+
+app.get(
+    '/auth/google', 
+    passport.authenticate('google', {
+        scope: ['profile', 'email']
+    })
+);
 
 const PORT = process.env.PORT
 app.listen(PORT)
-
